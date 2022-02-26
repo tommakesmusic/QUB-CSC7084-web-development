4f345bc14ecfe8e4725acc8271b549bdc0eea644
Tom <tommakesmusic@users.noreply.github.com>
2022-02-26 16:25:37 +0000

    More changes

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html class="no-js" lang="">
+<html>
 
 <head>
   <meta charset="utf-8">
@@ -26,9 +26,9 @@
 
   <!-- Add your site or application content here -->
   <p>Hello world! This is HTML5 Boilerplate.</p>
-  <script src="js/vendor/modernizr-3.11.2.min.js"></script>
-  <script src="js/plugins.js"></script>
-  <script src="js/main.js"></script>
+  <!-- <script src="js/vendor/modernizr-3.11.2.min.js"></script> -->
+  <!-- <script src="js/plugins.js"></script> -->
+  <!-- <script src="js/main.js"></script> -->
 
 <nav>
   <div class = "wrapper">
