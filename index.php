b527ccddf3bc510c5c9386f14fafb54562d3062b
Tom <tommakesmusic@users.noreply.github.com>
2022-02-26 17:03:03 +0000

    Finally putting things the way i want

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-  include_once 'header.php'
+  include_once 'html/header.html'
 ?>
   <section class ="album-intro">
     <h1>This is an introduction</h1>
@@ -16,5 +16,5 @@
 </section>
 
 <?php
-  include_once 'footer.php'
+  include_once 'html/footer.html'
 ?>
