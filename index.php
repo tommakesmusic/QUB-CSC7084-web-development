6fee47e386de65961a23f4534f5dc72445edcc94
Tom <tommakesmusic@users.noreply.github.com>
2022-02-26 15:20:21 +0000

    Starting to add login functionality

---
+++ index.php
@@ -0,0 +1,14 @@
+
+  <section class ="album-intro">
+    <h1>This is an introduction</h1>
+    <p>Here is some important information</p>
+  </section>
+<section class="album-genres">
+  <h2>Album Genres</h2>
+  <div class="album-genre-list">
+    <div><h3>Rock</h3></div>
+    <div><h3>Jazz</h3></div>
+    <div><h3>Pop</h3></div>
+    <div><h3>Metal</h3></div>
+  </div>
+</section>
