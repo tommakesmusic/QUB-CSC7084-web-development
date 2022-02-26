aeeb0a307e7d66f55c7f8031e918db947e55bff2
Tom <tommakesmusic@users.noreply.github.com>
2022-02-26 18:04:14 +0000

    Added a few css effects

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <section class="album-genres">
   <h2>Album Genres</h2>
   <div class="album-genre-list">
-  <div class="img-icon-settings"><h3>Rock</h3></div>
+  <div class="img-icon-settings"><h3 class="over-icon">Rock</h3></div>
     <div><h3>Jazz</h3></div>
     <div><h3>Pop</h3></div>
     <div><h3>Metal</h3></div>
