5655200de8d8cfae975e26b6873bdddaf20ff6e7
Tom <tommakesmusic@users.noreply.github.com>
2022-02-27 10:19:08 +0000

    Kevin Powell CSS Grid working

--- index.php
+++ index.php
@@ -1,19 +1,15 @@
 <?php
   include_once 'html/header.html'
 ?>
-  <section>
-    <h1>This is an introduction</h1>
-    <p>Here is some important information</p>
-  </section>
-<section class="genre-box">
-  <h2>Album Genres</h2>
 
-    <div><h3>Rock</h3></div>
-    <div><h3>Jazz</h3></div>
-    <div><h3>Pop</h3></div>
-    <div><h3>Metal</h3></div>
+<div class="content-genre-box grid-col-span-2 flow bg-primary-400 quote text-neutral-100">
+  <div class="flex"><h2>Album Genres</h2></div></div>
 
-</section>
+    <div class="content-genre-box flow bg-secondary-400 text-neutral-100"><h3>Rock</h3></div>
+    <div class="content-genre-box flow bg-secondary-500 text-neutral-100"><h3>Jazz</h3></div>
+    <div class="content-genre-box flow bg-secondary-400 text-neutral-100"><h3>Pop</h3></div>
+    <div class="content-genre-box flow bg-secondary-500 text-neutral-100"><h3>Metal</h3></div>
+
 
 <?php
   include_once 'html/footer.html'
