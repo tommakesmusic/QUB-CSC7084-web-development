c7564688ebc955173103590b0d2a0b1336f4cf17
Tom <tommakesmusic@users.noreply.github.com>
2022-02-27 10:22:37 +0000

    Adjusting look

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
 <div class="content-genre-box grid-col-span-2 flow bg-primary-400 quote text-neutral-100">
   <div class="flex"><h2>Album Genres</h2></div></div>
 
-    <div class="content-genre-box flow bg-secondary-400 text-neutral-100"><h3>Rock</h3></div>
-    <div class="content-genre-box flow bg-secondary-500 text-neutral-100"><h3>Jazz</h3></div>
-    <div class="content-genre-box flow bg-secondary-400 text-neutral-100"><h3>Pop</h3></div>
-    <div class="content-genre-box flow bg-secondary-500 text-neutral-100"><h3>Metal</h3></div>
+    <div class="content-genre-box flow bg-secondary-400 text-neutral-100"><div class="flex"><h3>Rock</h3></div></div>
+    <div class="content-genre-box flow bg-secondary-500 text-neutral-100"><div class="flex"><h3>Jazz</h3></div></div>
+    <div class="content-genre-box flow bg-neutral-100 text-secondary-500"><div class="flex"><h3>Pop</h3></div></div>
+    <div class="content-genre-box flow bg-secondary-500 text-neutral-100"><div class="flex"><h3>Metal</h3></div></div>
 
 
 <?php
