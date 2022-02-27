f5571dd53cd9074651459d5a7b179754a6887ea7
Tom <tommakesmusic@users.noreply.github.com>
2022-02-27 11:28:52 +0000

    Hoping its coming together

--- index.php
+++ index.php
@@ -2,14 +2,17 @@
   include_once 'html/header.html'
 ?>
 
-<div class="content-genre-box content-grid-col-span-2 flow bg-primary-400 quote text-neutral-100">
-  <div class="flex"><h2>Album Genres</h2></div></div>
+  <article class="content-genre-box content-grid-col-span-2 flow bg-primary-400 quote text-neutral-100">
+    <div class="flex"><h2>Album Genres</h2></div>
+</article>
 
-    <div class="content-genre-box flow bg-secondary-400 text-neutral-100"><div class="flex"><h3>Rock</h3></div></div>
-    <div class="content-genre-box flow bg-secondary-500 text-neutral-100"><div class="flex"><h3>Jazz</h3></div></div>
-    <div class="content-genre-box flow bg-neutral-100 text-secondary-500"><div class="flex"><h3>Pop</h3></div></div>
-    <div class="content-genre-box flow bg-secondary-500 text-neutral-100"><div class="flex"><h3>Metal</h3></div></div>
-
+  <article class="content-genre-box flow bg-secondary-400 text-neutral-100"><div class="flex"><h3>Rock</h3></div></article>
+  <article class="content-genre-box flow bg-secondary-500 text-neutral-100"><div class="flex"><h3>Jazz</h3></div></article>
+  <article class="content-genre-box flow bg-neutral-100 text-secondary-500"><div class="flex"><h3>Pop</h3></div></article>
+  <article class="content-genre-box flow bg-secondary-500 text-neutral-100"><div class="flex"><h3>Metal</h3></div></article>
+   <article class="content-genre-box flow bg-primary-400 text-neutral-100">
+    <div class="flex"><h2>Login for more Genres</h2></div>
+  </article>
 
 <?php
   include_once 'html/footer.html'
