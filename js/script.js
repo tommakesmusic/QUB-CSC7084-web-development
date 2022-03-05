5ed884aae0b41653e5f597407deb4e60ec1fde8d
Tom <tommakesmusic@users.noreply.github.com>
2022-03-05 13:16:02 +0000

    Add a temporary AJAX call

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Adding a random number function
+
+
+function myFunction(p1, p2) {
+    return p1 * p2;   // The function returns the product of p1 and p2
+  }
+
+// AJAX call
+
+  $(document).ready(function() {
+
+    $("#type").change(function() {
+        var val = $(this).val();
+
+        $.post('query.php', {'brand' : val}, function(data){
+            var jsonData = JSON.parse(data); // turn the data string into JSON
+            var newHtml = ""; // Initialize the var outside of the .each function
+            $.each(jsonData, function(item) {
+                newHtml += "<option>" + item['model'] + "</option>";
+            })
+            $("#size").html(newHtml);
+        });
+    });
+});
