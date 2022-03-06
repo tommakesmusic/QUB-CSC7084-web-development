24d72d8a8af110e3076858b1d8e5996d35e32df9
Tom <tommakesmusic@users.noreply.github.com>
2022-03-06 01:25:14 +0000

    Added helpers.php

--- php/user_model.php
+++ php/user_model.php
@@ -1,9 +1,10 @@
 // model.php
-// After https://www.youtube.com/watch?v=ATKDlgGrLzA and
-// https://www.youtube.com/watch?v=BcsgVhcltEM
+// After  https://www.youtube.com/watch?v=ATKDlgGrLzA and
+// part 2 https://www.youtube.com/watch?v=BcsgVhcltEM
 
 <?php
 include_once "./connection.php";
+include_once "./helpers.php";
 
 if ($_SERVER['REQUEST_METHOD'] == "POST" && $_POST['user_request'] == "signup"){
     signup($connection);
