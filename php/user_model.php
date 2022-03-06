bfed81cb3d985485b163aa4024e237f79373a69c
Tom <tommakesmusic@users.noreply.github.com>
2022-03-06 00:57:32 +0000

    starting a user rest api

---
+++ php/user_model.php
@@ -0,0 +1,39 @@
+// model.php
+// After https://www.youtube.com/watch?v=ATKDlgGrLzA and
+// https://www.youtube.com/watch?v=BcsgVhcltEM
+
+<?php
+include_once "./connection.php";
+
+if ($_SERVER['REQUEST_METHOD'] == "POST" && $_POST['user_request'] == "signup"){
+    signup($connection);
+}
+
+if ($_SERVER['REQUEST_METHOD'] == "POST" && $_POST['user_request'] == "login"){
+    login($connection);
+}
+
+if ($_SERVER['REQUEST_METHOD'] == "GET"){
+    logout($connection);
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] == "PATCH"){
+    update_user($connection);
+}
+
+if ($_SERVER['REQUEST_METHOD'] == "DELETE"){
+    remove_user($connection);
+} 
+
+// Functions
+
+function signup($connection){
+    
+};
+function login($connection){};
+function logout($connection){};
+function update_user($connection){};
+function remove_user($connection){};
+
+?>
