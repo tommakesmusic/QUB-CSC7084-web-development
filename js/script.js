c3e92f75bcaa7fbbedeccb7284a3c6e228c3ec12
Tom <tommakesmusic@users.noreply.github.com>
2022-03-06 14:25:05 +0000

    Changed to oo db connection plus some js

--- js/script.js
+++ js/script.js
@@ -7,6 +7,17 @@ function myFirstFunction() {
     document.getElementById("header").innerHTML = "My First JavaScript Change";
   }
 
+// User actions section
+const signup = document.querySelector('.signup');
+const login = document.querySelector('.login');
+const logout = document.querySelector('.logout');
+const update_user = document.querySelector('.update_user');
+const delete_user = document.querySelector('.delete_user');
+
+signup.addEventListener('click', ()=>{location.href="../html/signup.html"});
+login.addEventListener('click', ()=>{location.href="../html/login.html"});
+update_user.addEventListener('click', ()=>{location.href="../html/update_user.html"});
+
 // Generate a random number
 const random = (max = 4) => {
     return Math.floor(Math.random() * max);
--- php/connection.php
+++ php/connection.php
@@ -5,6 +5,16 @@
  $password="root";
  $database="Web_Test";
  
- $connection = mysqli_connect($hostname, $username, $password, $database); 
- 
+ // Two versions - both are here:
+ // $connection = mysqli_connect($hostname, $username, $password, $database); 
+
+$connection = new mysqli($hostname, $username, $password, $database);
+
+if ($connection ->connect_errno){
+    http_response_code(400);
+    header('content-type: text/plain');
+    echo $connection->connect_error;
+    exit();
+}
+
 ?>
