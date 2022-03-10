f80c62840111fc5cd09643c70223be71d0b0fef1
Tom <tommakesmusic@users.noreply.github.com>
2022-03-10 11:51:41 +0000

    Fix login form

--- userApi/login.php
+++ userApi/login.php
@@ -6,7 +6,7 @@ require_once '../html/headerSubFolder.html';
 
 <div class ="content-genre-box">
 <form onsubmit="return false;" autocomplete="on">
-    <div><label for ="">First name</label><input type="text" name="firstName" autocomplete="on"></div>
+    <div><label for ="">Username</label><input type="text" name="userName" autocomplete="on"></div>
     <div><label for ="">Password</label><input type="password" name="passWord" autocomplete="on"></div>
     <div><label for ="">Password</label><input type="password" name="passWordRpt" autocomplete="on"></div>
     <div><input type="hidden" name="userApiReq" value="login"></div>
